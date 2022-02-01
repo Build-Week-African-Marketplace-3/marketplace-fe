9c3a4d305b082e24074d3db607c6fea43735cf6b
Uros Ivanovic <urosivanovic@Uross-MBP-123.lan>
2022-01-31 21:08:32 -0500

    some Login styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,31 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import background from '../images/pic02.jpeg';
+
+const FormWrapper = styled.div`
+    background: no-repeat url(${background});
+    opacity: 0.8;
+    background-size: cover;
+    height: 100vh;
+    font-family: helvetica, sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const FormStyled = styled.div`
+    background-color: #fff;
+    height: 50vh;
+    weight: 25vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 60vh;
+    border-radius: 15px;
+    padding: 0 20px;
+`
+
 
 const Login = () => {
     const [user, setUser] = useState({
@@ -21,28 +48,30 @@ const Login = () => {
     return (
         <>
             <h1>Login</h1>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label> Username:
-                    <input
-                        type='text'
-                        name='username'
-                        value={user.username}
-                        onChange={handleChange} 
-                    />
-                    </label>
+            <FormWrapper>
+                <FormStyled>
+                    <form onSubmit={handleSubmit}>
+                        <label> Username:
+                        <input
+                            type='text'
+                            name='username'
+                            value={user.username}
+                            onChange={handleChange} 
+                        />
+                        </label>
 
-                    <label>Password:
-                    <input
-                        type='password'
-                        name='password'
-                        value={user.password}
-                        onChange={handleChange}    
-                    />
-                    </label>
-                    <button>Log In</button>
-                </form>
-            </div>
+                        <label>Password:
+                        <input
+                            type='password'
+                            name='password'
+                            value={user.password}
+                            onChange={handleChange}    
+                        />
+                        </label>
+                        <button>Log In</button>
+                    </form>
+                </FormStyled>
+            </FormWrapper>
         </>
     )
 }
